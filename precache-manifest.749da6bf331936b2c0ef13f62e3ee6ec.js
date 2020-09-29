2a3b5ce14611882756157453f02efe12b667581e
AdarshJaso <adarsh.jasoliya@gmail.com>
2020-09-29 11:45:13 +0530

    Updates

---
+++ precache-manifest.749da6bf331936b2c0ef13f62e3ee6ec.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d73ecb0bc994fb677f9622c9db7a7e2f",
+    "url": "/reactjs-todolist/index.html"
+  },
+  {
+    "revision": "068811d4f808ada816ed",
+    "url": "/reactjs-todolist/static/css/main.59c7dd3a.chunk.css"
+  },
+  {
+    "revision": "1b4dda4cff244f81c893",
+    "url": "/reactjs-todolist/static/js/2.3c57687a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactjs-todolist/static/js/2.3c57687a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "068811d4f808ada816ed",
+    "url": "/reactjs-todolist/static/js/main.ad258d19.chunk.js"
+  },
+  {
+    "revision": "58641caf3f22228e412b",
+    "url": "/reactjs-todolist/static/js/runtime-main.6b2f2bc0.js"
+  },
+  {
+    "revision": "2ed6ccfedd8589c755e45c686512ce57",
+    "url": "/reactjs-todolist/static/media/clear.2ed6ccfe.svg"
+  },
+  {
+    "revision": "27d8610109eef90ac99801803f6267d8",
+    "url": "/reactjs-todolist/static/media/edit.27d86101.svg"
+  },
+  {
+    "revision": "76d344f08412543c74ccd1fb2493d607",
+    "url": "/reactjs-todolist/static/media/github.76d344f0.svg"
+  },
+  {
+    "revision": "61d683536ed67f2a5b154afbe2180f58",
+    "url": "/reactjs-todolist/static/media/trash.61d68353.svg"
+  }
+]);
